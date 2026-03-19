1fbedc0368adce3992d7007eec7c251c6fea87e4
Fedor Romanov <fedor.romanov@outlook.com>
2026-03-19 16:31:08 +0100

    changed config to direct env usage in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(
-            at: config('app.trust_proxies', []),
+            at: explode(',', env('TRUST_PROXIES', [])),
             headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
